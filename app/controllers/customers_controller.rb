f638c22cde090b23cdf4f6e489e4d05c4d71a175
Maria Wissler <wmcarmelina@gmail.com>
2019-05-14 15:31:58 -0700

    added it works

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,21 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+    render json: customers.as_json(only: [:name, :registered_at, :adress, :city, :state, :postal_code, :phone]), status: :ok
+  end
+
+  def show
+    customer = Customer.find_by(id: params[:id])
+    if customer
+      render status: :ok, json: customer.as_json(only: [:name, :registered_at, :adress, :city, :state, :postal_code, :phone]), status: :ok
+    else
+      render status: :not_found, json: { errors: "Customer not found " }
+      # there could be more than one error
+    end
+  end
+
+  def zomg
+    render status: :not_found, json: { errors: "It works " }
+    # there could be more than one error
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'movies/index'
+  get "customers/index"
+
+  get "customers/zomg" => "customers#zomg"
+  resources "customers"
+
+  get "movies/index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
