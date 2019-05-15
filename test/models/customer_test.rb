49a5460b2e9545c0a47bf2115eeaf373c081275f
Maria Wissler <wmcarmelina@gmail.com>
2019-05-15 12:24:15 -0700

    initiated test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,57 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { customers(:chewy) }
 
-  it "must be valid" do
+  it "must be valid with all attributes included" do
     value(customer).must_be :valid?
+  end
+
+  it "must be invalid without a name" do
+    customer.name = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :name
+  end
+
+  it "must be invalid without a registered_at date" do
+    customer.registered_at = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :registered_at
+  end
+
+  it "must be invalid without an address" do
+    customer.address = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :address
+  end
+
+  it "must be invalid without a city" do
+    customer.city = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :city
+  end
+
+  it "must be invalid without a state" do
+    customer.state = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :state
+  end
+
+  it "must be invalid without a postal_code" do
+    customer.postal_code = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :postal_code
+  end
+
+  it "must be invalid without a phone" do
+    customer.phone = nil
+    value(customer).wont_be :valid?
+    customer.errors.messages.must_include :phone
+  end
+
+  describe "relations" do
+    it "must respond to rentals" do
+      customer.must_respond_to :rentals
+    end
   end
 end
