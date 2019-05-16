954c0d4b0f1dfd1b014bbf06595ab02ab9c47dbb
Maria Wissler <wmcarmelina@gmail.com>
2019-05-16 12:46:56 -0700

    updates-testing

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   resources :movies, only: [:index, :show, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "rentals/check-out", to: "rentals#check_out", as: "check_out"
-  post "rentals/available", to: "rentals#available", as: "available"
+  post "rentals/check_in", to: "rentals#check_in", as: "check_in"
 end
