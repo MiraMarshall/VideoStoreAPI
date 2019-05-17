1516a8b7eb45edf7cf1e9bf156cf2175f633799f
Maria Wissler <wmcarmelina@gmail.com>
2019-05-17 15:13:24 -0700

    Fixed checkout rental controller and inventory count

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,12 +2,21 @@ require "date"
 
 class RentalsController < ApplicationController
   def check_out
-    rental = Rental.create(rental_params)
-
+    rental = Rental.new(rental_params)
+    movie = Movie.find_by(id: params[:movie_id])
+    if movie.available_inventory == 0
+      render status: :not_found, json: { ok: false, mesages: "Movie not found" }
+    else
+      movie.available_inventory -= 1
+    end
     rental.check_out = DateTime.now.to_date
     rental.due_date = rental.check_out + 7
 
     if rental.save
+      movie.save
+      customer = Customer.find_by(id: rental.customer_id)
+      customer.movies_checked_out_count += 1
+      customer.save
       render status: :ok, json: { id: rental.id }
     else
       render status: :bad_request, json: { ok: false, errors: rental.errors }
@@ -15,13 +24,15 @@ class RentalsController < ApplicationController
   end
 
   def check_in
+    rental = Rental.where(["movie_id = ? and customer_id = ? and check_in = ?", params[:rental][:movie_id], params[:rental][:customer_id]]).first
     customer = Customer.find_by(id: params[:customer_id])
     movie = Movie.find_by(id: params[:movie_id])
-    rental = Rental.find_by(customer_id: customer.id, movie_id: movie.id)
+
+    rental.save
 
     if rental
       movie.update(available_inventory: movie.available_inventory += 1)
-      # customer.movies_checked_out -= 1
+      customer.movies_checked_out -= 1
       rental.check_in = Date.today
 
       render json: { id: rental.id }, status: :ok
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-JSON.parse(File.read('db/seeds/customers.json')).each do |customer|
+JSON.parse(File.read("db/seeds/customers.json")).each do |customer|
   Customer.create!(customer)
+  current_customer = Customer.last
+  current_customer.movies_checked_out_count = 0
+  current_customer.save
 end
 
-JSON.parse(File.read('db/seeds/movies.json')).each do |movie|
+JSON.parse(File.read("db/seeds/movies.json")).each do |movie|
   Movie.create!(movie)
+  current_movie = Movie.last
+  current_movie.available_inventory = current_movie.inventory
+  current_movie.save
 end
