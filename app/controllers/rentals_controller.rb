40768969baabb174058194c80084065e18105b11
Maria Wissler <wmcarmelina@gmail.com>
2019-05-17 12:25:24 -0700

    updated model relationship

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,14 +1,17 @@
 require "date"
+require "pry"
 
 class RentalsController < ApplicationController
   def check_out
-    rental = Rental.new(rental_params)
+    rental = Rental.create(rental_params)
+
     rental.check_out = DateTime.now.to_date
     rental.due_date = rental.check_out + 7
     rental.save
+    # binding.pry
 
     if rental
-      render status: :ok, json: rental.as_json(only: [:id, :customer_id, :movie_id, :due_date])
+      render status: :ok, json: { id: rental.id }
     else
       render status: :bad_request, json: { ok: false, errors: rental.errors }
     end
@@ -17,13 +20,13 @@ class RentalsController < ApplicationController
   def check_in
     customer = Customer.find_by(id: params[:customer_id])
     movie = Movie.find_by(id: params[:movie_id])
-    rental = Rental.find_by(customer_id: customer, movie_id: movie.id)
+    rental = Rental.find_by(customer_id: customer.id, movie_id: movie.id)
     #binding.pry
 
     if rental
-      # movie.inventory += 1
+      movie.update(available_inventory: movie.available_inventory += 1)
       # customer.movies_checked_out -= 1
-      # rental.check_in = Date.today
+      rental.check_in = Date.today
 
       render json: { id: rental.id }, status: :ok
     else
