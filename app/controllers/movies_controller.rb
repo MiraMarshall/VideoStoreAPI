3ee08dfc61120e8d29c2739f844959aaf5a68bdc
Maria Wissler <wmcarmelina@gmail.com>
2019-05-16 18:24:51 -0700

    test for postman

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,9 +4,33 @@ class MoviesController < ApplicationController
     render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
 
+  def show
+    movie = Movie.find_by(id: params[:id])
+
+    if !movie.nil?
+      render json: movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory]),
+        status: :ok
+    else
+      render json: { ok: false, errors: ["Movie not found"] },
+        status: :not_found
+    end
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory]),
+        status: :ok
+    else
+      render json: { ok: false, errors: movie.errors.messages },
+        status: :bad_request
+    end
+  end
+
   private
 
   def params
-    params.require(:movie).permit(:title, :release_date, :inventory)
+    params.permit(:title, :overview, :release_date, :inventory)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get "customers/zomg" => "customers#zomg"
   resources "customers"
 
-  get "movies/index"
-  resources :movies, only: [:index, :show, :create]
+  # get "movies/index"
+  resources :movies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post "rentals/check-out", to: "rentals#check_out", as: "check_out"
   post "rentals/check_in", to: "rentals#check_in", as: "check_in"
