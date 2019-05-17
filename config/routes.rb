a8963221d7ecd0d2e7cd44c14dd009765d3e21e2
Shamira <marshallscm@gmail.com>
2019-05-17 14:10:34 -0700

    passing customer controller tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :customers, only: [:index]
+  resources :customers, only: [:index, :show]
   resources :movies, only: [:index, :show, :create]
   post "rentals/check-out", to: "rentals#check_out", as: "rentals_check_out"
   post "rentals/check-in", to: "rentals#check_in", as: "rentals_check_in"
