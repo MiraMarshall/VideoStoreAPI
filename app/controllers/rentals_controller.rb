73840d9153406b48511b4a53a4407b836178f8c2
Maria Wissler <wmcarmelina@gmail.com>
2019-05-18 22:03:57 -0700

    passing all smoke test, yay

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -28,15 +28,17 @@ class RentalsController < ApplicationController
 
   def check_in
     rental = Rental.where(["movie_id = ? and customer_id = ?", params[:rental][:movie_id], params[:rental][:customer_id]]).first
-    customer = Customer.find_by(id: params[:rental][:customer_id])
-    movie = Movie.find_by(id: params[:rental][:movie_id])
     rental.check_in = Date.today
 
     if rental.save
-      customer.movies_checked_out_count -= 1
-      customer.save
-      movie.available_inventory += 1
-      movie.save
+      customer = Customer.find_by(id: rental.customer_id)
+      movie = Movie.find_by(id: rental.movie_id)
+
+      customer_movie_count = customer.movies_checked_out_count
+      customer.update(movies_checked_out_count: customer_movie_count - 1)
+
+      movie_inventory = movie.available_inventory
+      movie.update(available_inventory: movie_inventory + 1)
 
       render json: { id: rental.id }, status: :ok
     else
