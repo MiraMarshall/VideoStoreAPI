2d0a617872e60253d207766a0dccac9b0f853e36
Maria Wissler <wmcarmelina@gmail.com>
2019-05-18 21:52:06 -0700

    checkout count working

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,20 +3,23 @@ require "date"
 class RentalsController < ApplicationController
   def check_out
     rental = Rental.new(rental_params)
-    movie = Movie.find_by(id: params[:movie_id])
+    movie = Movie.find_by(id: rental.movie_id)
     if movie.available_inventory == 0
       render status: :not_found, json: { ok: false, mesages: "Movie not found" }
-    else
-      movie.available_inventory -= 1
     end
+
     rental.check_out = DateTime.now.to_date
     rental.due_date = rental.check_out + 7
 
     if rental.save
-      movie.save
       customer = Customer.find_by(id: rental.customer_id)
-      customer.movies_checked_out_count += 1
-      customer.save
+
+      customer_movie_count = customer.movies_checked_out_count
+      customer.update(movies_checked_out_count: customer_movie_count + 1)
+
+      movie_inventory = movie.available_inventory
+      movie.update(available_inventory: movie_inventory - 1)
+
       render status: :ok, json: { id: rental.id }
     else
       render status: :bad_request, json: { ok: false, errors: rental.errors }
@@ -27,8 +30,8 @@ class RentalsController < ApplicationController
     rental = Rental.where(["movie_id = ? and customer_id = ?", params[:rental][:movie_id], params[:rental][:customer_id]]).first
     customer = Customer.find_by(id: params[:rental][:customer_id])
     movie = Movie.find_by(id: params[:rental][:movie_id])
-
     rental.check_in = Date.today
+
     if rental.save
       customer.movies_checked_out_count -= 1
       customer.save
